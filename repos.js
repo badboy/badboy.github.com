d041f5cbcd10c3d2ac3d2094fab73ffb23075598
BadBoy_ <psp.badboy@gmail.com>
2009-02-15 00:32:24 +0100

    changesin repo.js and index.html

--- repos.js
+++ repos.js
@@ -1,5 +1,5 @@
 $(function() {
-	var limit = 10        // how many repos to list
+	var limit = 20        // how many repos to list
 	var login = 'badboy' // your username
 
 	$.getJSON('http://github.com/api/v1/json/' + login + '?callback=?', function(data) {
@@ -11,12 +11,6 @@ $(function() {
 			return a.watchers - b.watchers
 		})
 		
-		//repos = $.makeArray(repos);
-		//alert(repos[0].url);
-		//var nrepos = $.map(repos, function(e) {
-		//	return '<li><a href="' + e.url + '">&quot;' + e.name + '&quot;</a></li>'
-		//})
-		//alert(nrepos[0]);
 		var i = 0;
 		var len = repos.length
 		if(len > limit) len = limit;
