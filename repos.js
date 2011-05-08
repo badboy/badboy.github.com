e11433bdbf5d6e2e3f84999313eebdd7779a1e74
Jan-Erik Rediger <badboy@archlinux.us>
2011-05-08 22:22:41 +0000

    update with correct data, newer api version and ruby1.9 hashes

--- repos.js
+++ repos.js
@@ -1,27 +1,26 @@
 $(function() {
-	var limit = 20        // how many repos to list
-	var login = 'badboy' // your username
+	var limit = 20;       // how many repos to list
+	var login = 'badboy'; // your username
 
-	$.getJSON('http://github.com/api/v1/json/' + login + '?callback=?', function(data) {
-		var repos = $.grep(data.user.repositories, function() {
-			return !this.fork
-		})
+	$.getJSON('http://github.com/api/v2/json/repos/show/' + login + '?callback=?', function(data) {
+		var repos = $.grep(data.repositories, function(repo) {
+			return !repo.fork;
+		});
 
 		repos.sort(function(a, b) {
-			return a.watchers - b.watchers
-		})
+			return ( (+new Date(a.pushed_at)) < (+new Date(b.pushed_at)) ? 1 : -1 );
+		});
 		
-		var i = 0;
-		var len = repos.length
+		var len = repos.length;
 		if(len > limit) len = limit;
 
 		$('#repos').text('');
 		$.each(repos.slice(0, len), function(ind, val) {
             if(val.name == 'badboy.github.com') return;
 			if(ind+1 == len)
-				$('#repos').append('<li><a href="' + val.url + '">&quot;' + val.name + '&quot;</a></li>')
+				$('#repos').append('<li><a href="' + val.url + '">&quot;' + val.name + '&quot;</a></li>');
 			else
-				$('#repos').append('<li><a href="' + val.url + '">&quot;' + val.name + '&quot;</a>,</li>')
-		})
-	})
-})
+				$('#repos').append('<li><a href="' + val.url + '">&quot;' + val.name + '&quot;</a>,</li>');
+		});
+	});
+});
