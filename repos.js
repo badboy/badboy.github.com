1448430c7c08f51910fbef875b18f6a98b0e7aaa
Jan-Erik Rediger <badboy@archlinux.us>
2014-01-10 00:58:26 +0100

    Fetch already sorted data.

--- repos.js
+++ repos.js
@@ -2,10 +2,8 @@ $(function() {
   var limit = 10;       // how many repos to list
   var login = 'badboy'; // your username
 
-  $.getJSON('https://api.github.com/users/' + login + '/repos?callback=?', function(data) {
-    var repos = $.grep(data.data, function(repo) {
-      return !repo.fork;
-    });
+  $.getJSON('https://api.github.com/users/' + login + '/repos?sort=pushed&callback=?', function(data) {
+    var repos = data.data
 
     repos.sort(function(a, b) {
       return ( (+new Date(a.updated_at)) < (+new Date(b.updated_at)) ? 1 : -1 );
